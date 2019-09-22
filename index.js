0a3188ef9b4664e57c09bbecb481c77c852f5df9
Ahmad AlZoubi <a.zlzoubi@nas.sy>
2019-09-22 11:11:19 +0300

    add the project

---
+++ index.js
@@ -0,0 +1,120 @@
+const Joi = require('@hapi/joi')
+const express = require('express')
+const logger = require('./logger')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(logger)
+
+let courses = [
+    {
+        id: 1,
+        name: 'js'
+    },
+    {
+        id: 2,
+        name: 'html'
+    },
+    {
+        id: 3,
+        name: 'css'
+    }
+]
+
+
+function validateData(data) {
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(10).required()
+    })
+    const result = schema.validate(data)
+    return result
+}
+
+app.get('/', (req, res) => {
+    res.status(200).send(req.body)
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (course) {
+        res.status(200)
+        res.send(course)
+    }
+    else {
+        res.status(404)
+        res.send('not found')
+    }
+})
+
+app.post('/api/courses', (req, res) => {
+
+    const { error } = validateData(req.body)
+
+    if (error) {
+        res.status(400).send(error.details[0].message)
+    }
+    else {
+        const course = {
+            id: courses.length + 1,
+            name: req.body.name
+        }
+
+        courses.push(course)
+
+        res.status(200).send(courses)
+    }
+
+})
+
+app.put('/api/courses/:id', (req, res) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) {
+        res.status(404)
+        res.send('not found')
+        return
+    }
+
+    const { error } = validateData(req.body)
+    if (error) {
+        res.status(400).send(error.details[0].message)
+    }
+    else {
+
+        course.name = req.body.name
+        res.status(200).send({
+            courses,
+        })
+    }
+
+})
+
+app.delete('/api/courses/:id', (req, res) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) {
+        res.status(404)
+        res.send('not found')
+        return
+    }
+
+    const index = courses.indexOf(course)
+    const str = courses.splice(index, 1)
+    console.log(str)
+    
+    res.status(200)
+    res.send(courses)
+})
+
+
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`running on port ${port}...`))
