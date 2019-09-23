11fb6a16889ba69e981d80403d5098faeb450d74
ahmad <ahmadalzoubi13579@gmail.com>
2019-09-23 19:15:21 +0300

    finish async code

--- test/callbacks.js
+++ test/callbacks.js
@@ -1,14 +1,29 @@
-getUser(1).then(user => getRepos(user))
-    .then(user => getCommits(user))
-    .then(user => console.log(user))
-    .catch(err => console.log(`Error: ${err.message}`))
+// getUser(1).then(user => getRepos(user))
+//     .then(user => getCommits(user))
+//     .then(user => console.log(user))
+//     .catch(err => console.log(`Error: ${err.message}`))
+
+async function getData() {
+    try {
+        const user = await getUser(1)
+        const repos = await getRepos(user)
+        const commits = await getCommits(repos)
+        console.log(commits);
+    }
+    catch (err) {
+        console.log(`Error: ${err.message}`);
+    }
+}
+
+getData()
+console.log('here');
 
 
 function getUser(id) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('getting user from database');
-            resolve({id: id, usernmae: 'ahmad'})
+            resolve({ id: id, usernmae: 'ahmad' })
         }, 1000)
     })
 }
@@ -17,7 +32,7 @@ function getRepos(user) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('getting Repos from database');
-            resolve({...user, repos:['html', 'js', 'css']})
+            resolve({ ...user, repos: ['html', 'js', 'css'] })
         }, 1000)
     })
 }
@@ -26,7 +41,7 @@ function getCommits(user) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('getting commits from database');
-            resolve({...user, commits: [1,2,3,4,5]})
+            resolve({ ...user, commits: [1, 2, 3, 4, 5] })
         }, 1000)
     })
 }
--- test/promise-api.js
+++ test/promise-api.js
@@ -1,4 +1,16 @@
-// const p = Promise.resolve({ id: 1 })
-const p = Promise.reject(new Error('username error'))
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('task 1');
+        resolve(1)
+    }, 2000)
+})
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('task 2');
+        // resolve(2)
+        reject(new Error('message'))
+    }, 2000)
+})
 
-p.catch(err => console.log(err))
+Promise.race([p1, p2]).then(result => console.log(result))
+       .catch(err => console.log(err.message))
